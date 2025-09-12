37e80a22055613966aa3052af6b517ee258a9ac0
Olha Pelypets <olha.pelypets@welltech.com>
2025-09-13 01:27:32 +0200

    Compose Utils

---
+++ front/app/src/main/kotlin/ui/utils/ComposeUtils.kt
@@ -0,0 +1,66 @@
+package com.manager1700.soccer.ui.utils
+
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Modifier.navBarBottomPadding() =
+    this.padding(WindowInsets.navigationBars.asPaddingValues())
+
+@Composable
+fun Modifier.statusBarTopPadding() =
+    this.padding(WindowInsets.statusBars.asPaddingValues())
+
+val cardBrushGradient = Brush.verticalGradient(
+    colors = listOf(
+        Color(0xFF1C273D),
+        Color(0xFF2A3441)
+    )
+)
+
+val cardBrushLightGradient = Brush.verticalGradient(
+    colors = listOf(
+        Color(0xFF283E6D),
+        Color(0xFF283E6D)
+    )
+)
+
+val cardBrushLightLightGradient = Brush.verticalGradient(
+    colors = listOf(
+        Color(0xFF202B45),
+        Color(0xFF0270D5)
+    )
+)
+
+val cardActiveBrushGradient = Brush.verticalGradient(
+    colors = listOf(
+        Color(0xFFD361FE),
+        Color(0xFF78AAFB)
+    )
+)
+
+val blueButtonBrushLightGradient = Brush.verticalGradient(
+    colors = listOf(
+        Color(0xFF0084EA),
+        Color(0xFF0368CD)
+    )
+)
+
+val yellowButtonBrushLightGradient = Brush.verticalGradient(
+    colors = listOf(
+        Color(0xFFFADD53),
+        Color(0xFFFEBB08)
+    )
+)
+
+val cardClipShape = RoundedCornerShape(8.dp)
+val buttonClipShape = RoundedCornerShape(16.dp)
