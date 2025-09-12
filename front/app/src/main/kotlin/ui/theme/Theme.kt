c046b799331bd64d2ce22dbd5a5c8c51bc8b5ec4
Olha Pelypets <olha.pelypets@welltech.com>
2025-09-13 01:22:09 +0200

    Colors

--- front/app/src/main/kotlin/ui/theme/Theme.kt
+++ front/app/src/main/kotlin/ui/theme/Theme.kt
@@ -27,7 +27,6 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun SoccerManagerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
