976a4624568b8d4a28d45d4564d0caadc88475f9
Olha Pelypets <olha.pelypets@welltech.com>
2025-09-15 12:14:24 +0200

    Player status chip

--- front/app/src/main/kotlin/ui/components/Chips.kt
+++ front/app/src/main/kotlin/ui/components/Chips.kt
@@ -11,8 +11,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.manager1700.soccer.Montserrat
+import com.manager1700.soccer.domain.models.PlayerStatus
+import com.manager1700.soccer.ui.theme.colorGrey_2b
 import com.manager1700.soccer.ui.theme.colorLightGreen
 import com.manager1700.soccer.ui.theme.colorLightPink
 import com.manager1700.soccer.ui.theme.colorYellow
@@ -53,3 +58,33 @@ fun FitnessChipPreview() {
         }
     }
 }
+
+@Composable
+fun PlayerStatusChip(value: PlayerStatus) {
+    val color = if (value == PlayerStatus.Active) {
+        colorLightGreen
+    } else {
+        colorLightPink
+    }
+    Text(
+        text = stringResource(value.titleId),
+        color = colorGrey_2b,
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        modifier = Modifier
+            .clip(RoundedCornerShape(4.dp))
+            .background(color)
+            .padding(vertical = 2.dp, horizontal = 8.dp)
+    )
+}
+
+@Preview
+@Composable
+fun PlayerStatusChipPreview() {
+    Column(
+    ) {
+        PlayerStatusChip(PlayerStatus.Active)
+        PlayerStatusChip(PlayerStatus.Injured)
+    }
+}
+
